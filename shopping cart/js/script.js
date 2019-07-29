7cbdd1fd74c84745062922be0c5dd384ef4496d2
Darko Kolak <kolakdarko@yahoo.com>
2019-07-29 11:56:00 +0200

    done

--- shopping cart/js/script.js
+++ shopping cart/js/script.js
@@ -6,6 +6,41 @@
 	// 	document.getElementById("outerbox").innerHTML = '</div>';
 	// }
 
+
 	for (var i = 0; i < cars.length; i++) {
-		document.getElementById("outerbox").innerHTML += `<div class="innerboxes"><img src="${cars[i].image}"><p>${cars[i].name}</p><p>Production Year: ${cars[i].productionYear}</p><p>Price: ${cars[i].price}</p><button id="${i}">Add to Cart</button></div>`;
+		document.getElementById("outerbox").innerHTML += `<div class="innerboxes"><img src="${cars[i].image}"><p>${cars[i].name}</p><p>Production Year: ${cars[i].productionYear}</p><p>Price: ${cars[i].price}</p><button id="${i}" class = "tocart">Add to Cart</button></div>`;
+	}
+
+
+var allbtn = document.getElementsByClassName("tocart");
+
+		 for (var i =0;  i< cars.length; i++) {
+	allbtn[i].addEventListener("click", function(){add(this.getAttribute("id"))},false);
+}
+
+
+
+function add(i){
+
+
+ document.getElementById("result").innerHTML += `<p> ${cars[i].name} </p>` ;
+
+}
+
+var buttonright = document.getElementById("shoppingcart");
+
+buttonright.addEventListener("click",viewcart,false);
+
+function viewcart(){
+
+	if (result.style.display === "none") {
+
+		result.style.display = "block";
+		buttonright.innerHTML = "Hide Cart";
+	}
+
+	else{
+		result.style.display = "none";
+		buttonright.innerHTML = "View Cart";
+	}
 }
