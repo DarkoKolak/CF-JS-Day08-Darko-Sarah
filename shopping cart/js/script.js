d063f7521c85b22edd47b08565a9f9f69fb31c38
Sarah Berger <berger-sarah@hotmail.com>
2019-07-29 11:12:32 +0200

    *create json *add shopping cart field and button

--- shopping cart/js/script.js
+++ shopping cart/js/script.js
@@ -1,32 +1,3 @@
-class Car {
-	constructor(name,productionYear,image,price){
-		this.name = name;
-		this.productionYear = productionYear;
-		this.image = image;
-		this.price = price;
-	}
-}
-
-	var car1 = new Car("Jaguar Roadster XK","1956", "jaguar.jpg", 50);
-
-	var car2 = new Car("Tesla Roadster","2020", "tesla.jpg", 60);
-
-	var car3 = new Car("Corvette C1","1955", "corvette.jpg", 70);
-
-	var car4 = new Car("Rolls Royce Silver Dawn","1954", "royce.jpg", 89);
-
-	var car5 = new Car("VW Beetle","1938", "vw.jpg", 81);
-
-	var car6 = new Car("DeLorean DMC-12", "1981", "delorean.jpg", 73);
-
-	var car7 = new Car("Audi E-Tron GT", "2020", "audi.jpg", 90);
-
-	var car8 = new Car("Ford Shelby GT", "1967", "ford.jpg", 74);
-
-	var car9 = new Car("Millennium Falcon", "1977", "falcon.jpg", 109);
-
-	var carArray = [car1, car2, car3, car4, car5, car6, car7, car8, car9];
-
 
 	// for (var i = 0; i < carArray.length; i++) {
 	// 	document.getElementById("outerbox").innerHTML = '<div class=\"innerboxes\">';
@@ -35,6 +6,6 @@ class Car {
 	// 	document.getElementById("outerbox").innerHTML = '</div>';
 	// }
 
-	for (var i = 0; i < carArray.length; i++) {
-		document.getElementById("outerbox").innerHTML += `<div class="innerboxes"><img src="` + carArray[i].image + '\"><p>'+ carArray[i].name + '</p></div>';
+	for (var i = 0; i < cars.length; i++) {
+		document.getElementById("outerbox").innerHTML += `<div class="innerboxes"><img src="${cars[i].image}"><p>${cars[i].name}</p><p>Production Year: ${cars[i].productionYear}</p><p>Price: ${cars[i].price}</p><button id="${i}">Add to Cart</button></div>`;
 }
